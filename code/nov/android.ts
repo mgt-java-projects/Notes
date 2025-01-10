e64a8a5920ad6be6fbee8cabd9aca5dc5acbb460
ManiyamGT <79772011+ManiyamGT@users.noreply.github.com>
2025-01-10 13:58:39 -0500

    devic

--- code/nov/android.ts
+++ code/nov/android.ts
@@ -1,30 +1,17 @@
-import android.net.http.SslError;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // Allow insecure SSL connections (only for development)
-        this.getBridge().getWebView().setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed(); // Ignore SSL errors for development
-            }
-        });
-    }
-}
+// Action to update partial contact info
+export const updateContactInfo = createAction(
+    '[Contact] Update Contact Info',
+    props<{ phone?: string }>() // Partial update for phone
+  );
 
 
-import android.webkit.WebSettings;
+   // Handle updating partial contact info (only phone in this case)
+   on(updateContactInfo, (state, { phone }) => ({
+    ...state,
+    ...(phone ? { phone } : {}), // Update only if phone is provided
+  }))
 
-@Override
-public void onStart() {
-    super.onStart();
 
-    this.getBridge().getWebView().getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-}
+  updatePhone() {
+    this.store.dispatch(updateContactInfo({ phone: '0987654321' }));
+  }
